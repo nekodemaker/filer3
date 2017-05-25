4f2823e15369c0479ef014ca76e883e70d221d94
Sodara NHEK <sodara.nhek@supinternet.fr>
2017-05-25 03:01:06 +0200

    filer v2 to object : edit file text done

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -25,12 +25,20 @@ class DefaultController extends BaseController
             }else{
                 $message="";
             }
+            if(!empty($_SESSION['open-file-edit'])){
+                $openFileEdit=$_SESSION['open-file-edit'];
+                $fileGetContentsOpenFileEdit=file_get_contents($_SESSION['open-file-edit']);
+                unset($_SESSION['open-file-edit']);
+            }else{
+                $openFileEdit="";
+                $fileGetContentsOpenFileEdit="";
+            }
             $fileManager = FileManager::getInstance();
             $directoryManager = DirectoryManager::getInstance();
             $allFiles=$fileManager->getAllFilesForDisplay($_SESSION['id']);
             $allDirectories=$directoryManager->get_all_directories_by_id($_SESSION['id']);;
             echo $this->renderView('home.html.twig',
-            ['id' => $_SESSION['id'],'username'=>$_SESSION['username'],'error'=>$error,'message'=>$message,'allFiles'=>$allFiles,'allDirectories'=>$allDirectories]);
+            ['id' => $_SESSION['id'],'username'=>$_SESSION['username'],'error'=>$error,'message'=>$message,'openFileEdit'=>$openFileEdit,'fileGetContentsOpenFileEdit'=>$fileGetContentsOpenFileEdit,'allFiles'=>$allFiles,'allDirectories'=>$allDirectories]);
         }
         else {
             echo $this->renderView('home.html.twig',
